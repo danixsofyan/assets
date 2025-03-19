c26020a37e6b19245914f9b923ddca60dbc804f2
Dani Sofyan <danixsofyan@gmail.com>
2025-03-19 16:44:50 +0700

    Menu configuration

---
+++ app/Filament/Resources/LocationResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LocationResource\Pages;
+use App\Filament\Resources\LocationResource\RelationManagers;
+use App\Models\Location;
+use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LocationResource extends Resource
+{
+    protected static ?string $model = Location::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-flag';
+
+    protected static ?string $navigationGroup = 'Configuration';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()->schema([
+                    TextInput::make('branch_name')->required()->label('Cabang'),
+                    TextInput::make('building_name')->required()->label('Gedung'),
+                    TextInput::make('floor')->required()->label('Lantai'),
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('branch_name')->label('Cabang')->searchable(),
+                TextColumn::make('building_name')->label('Gedung')->searchable(),
+                TextColumn::make('floor')->label('Lantai')->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLocations::route('/'),
+            'create' => Pages\CreateLocation::route('/create'),
+            'edit' => Pages\EditLocation::route('/{record}/edit'),
+        ];
+    }
+}
